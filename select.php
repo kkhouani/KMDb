c5fa282b1212d26fd52175a939557533479f27d4
Khalid el Khouani <kkhouani@rhinofly.nl>
2015-02-13 15:53:38 +0100

    added faceted search

--- select.php
+++ select.php
@@ -172,11 +172,7 @@ if (!empty($_GET['minruntime']) && !empty($_GET['maxruntime'])) {
 
 		<div class="inner-wrap">
 			<input id="search" type="text" name="search" onkeyup="getMovies(this.value)" placeholder="Search movie" />
-			<div id="results"></div>
-		</div>
-
-		<div class="inner-wrap">
-			<input id="search" type="text" name="search" onkeyup="getInfo(this.value)" placeholder="Search movie" />
+			<input id="search" type="text" name="search" onkeyup="getInfo(this.value)" placeholder="Search director" />
 			<div id="results"></div>
 		</div>
 	</nav>
